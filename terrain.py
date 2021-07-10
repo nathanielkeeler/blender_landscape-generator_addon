c0772931f3d178ff92abb1b3fce6dee2255a2f30
Nathan <nathaniel.hermann.keeler@hs-furtwangen.de>
2021-07-10 16:20:05 +0200

    adds sky and world lighting to terrain.py

--- terrain.py
+++ terrain.py
@@ -1,6 +1,7 @@
 import bpy
 import random
 import typing
+from bpy.types import Nodes
 from mathutils import Vector
 
 class terrain():
@@ -8,6 +9,12 @@ class terrain():
     bpy.ops.object.select_all(action='SELECT') # selektiert alle Objekte
     bpy.ops.object.delete(use_global=False, confirm=False) # löscht selektierte objekte
     bpy.ops.outliner.orphans_purge() # löscht überbleibende Meshdaten etc.
+
+    # Delete world nodes
+    world_nodes = bpy.data.worlds['World'].node_tree
+
+    for currentNode in world_nodes.nodes:
+        world_nodes.nodes.remove(currentNode)
 
     def generate_terrain(self):
         bpy.context.scene.render.engine = 'CYCLES'
@@ -67,7 +74,66 @@ class terrain():
         mat_terrain.node_tree.links.new(node_color_ramp.outputs[0], node_terrain_displace.inputs[0])
         mat_terrain.node_tree.links.new(node_terrain_displace.outputs[0], node_output.inputs[2])
 
-        plane.data.materials.append(mat_terrain)      
+        plane.data.materials.append(mat_terrain)
+
+    def add_lighting(self):
+        light_data = bpy.data.lights.new('light', type='SUN')
+        light = bpy.data.objects.new('light', light_data)
+        bpy.context.collection.objects.link(light)
+        light.location = (2.5, 2.5, 2.5)
+        light.data.energy = 1.5
+
+    def add_sky(self):
+        world = bpy.data.worlds['World']
+        world.use_nodes = True
+
+        nodes_world: typing.List[bpy.types.Node] = world.node_tree.nodes
+
+        node_output: bpy.types.Node = nodes_world.new("ShaderNodeOutputWorld")
+        node_output.location = Vector((200, 300))
+
+        node_background: bpy.types.Node = nodes_world.new("ShaderNodeBackground")
+        node_background.location = Vector((0, 300))
+
+        # Color Node
+        node_sky: bpy.types.Node = nodes_world.new("ShaderNodeValToRGB")
+        node_sky.location = Vector((-300, 300))
+        node_sky.color_ramp.interpolation = 'B_SPLINE'
+        node_sky.color_ramp.elements.new(0.1)
+        node_sky.color_ramp.elements[0].position = 0.13
+        node_sky.color_ramp.elements[0].color = (0.062, 0.14, 0.38, 1)
+        node_sky.color_ramp.elements[1].color = (0.14, 0.23, 0.5, 1)
+        node_sky.color_ramp.elements[1].position = 0.27
+        node_sky.color_ramp.elements[2].position = 0.48
+
+        node_gradient: bpy.types.Node = nodes_world.new("ShaderNodeTexGradient")
+        node_gradient.location = Vector((-500, 300))
+        
+        node_map: bpy.types.Node = nodes_world.new("ShaderNodeMapping")
+        node_map.location = Vector((-700, 300))
+        node_map.inputs[1].default_value[0] = 0.28
+        node_map.inputs[2].default_value[1] = -1.5708
+        
+        node_tex_coord: bpy.types.Node = nodes_world.new("ShaderNodeTexCoord")
+        node_tex_coord.location = Vector((-900, 300))
+
+        # Links
+        links = world.node_tree.links
+        links.new(node_background.outputs[0], node_output.inputs[0])
+        links.new(node_sky.outputs[0], node_background.inputs[0])
+        links.new(node_gradient.outputs[1], node_sky.inputs[0])
+        links.new(node_map.outputs[0], node_gradient.inputs[0])
+        links.new(node_tex_coord.outputs[0], node_map.inputs[0])
+
+        # Ray Settings
+        world.cycles_visibility.scatter = False
+        world.cycles_visibility.transmission = False
+        world.cycles_visibility.glossy = False
+        world.cycles_visibility.diffuse = False
+
+
 
 t = terrain()    
 t.generate_terrain()
+t.add_lighting()
+t.add_sky()
